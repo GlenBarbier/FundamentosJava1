e9db99f9e2963395645f68de6f2967742dcc9078
GlenBarbier <glendabarbiero@gmail.com>
2023-10-25 23:48:10 -0300

    Primer commit

---
+++ src/fundamentos/java/pkg1/FundamentosJava1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundamentos.java.pkg1;
+
+/**
+ *
+ * @author Glendita
+ */
+public class FundamentosJava1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //BUCLES
+        /*
+        
+Los bucles `for` y `while` en Java se utilizan para la repetición de código:
+
+- `for`: Ideal para realizar un número conocido de iteraciones. Se compone de inicialización, condición y actualización.
+
+- `while`: Utilizado cuando no se conoce el número exacto de iteraciones. Repite mientras se cumple una condición booleana.
+
+Ambos bucles son fundamentales para controlar el flujo de ejecución en programas Java.
+        */
+        
+        
+    }
+    
+}
