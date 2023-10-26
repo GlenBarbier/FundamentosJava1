181c95be4692e770c0013425d331a4b75941f98a
GlenBarbier <glendabarbiero@gmail.com>
2023-10-26 00:09:10 -0300

    Variables

--- src/fundamentos/java/pkg1/FundamentosJava1.java
+++ src/fundamentos/java/pkg1/FundamentosJava1.java
@@ -27,7 +27,24 @@ Los bucles `for` y `while` en Java se utilizan para la repetición de código:
 - `while`: Utilizado cuando no se conoce el número exacto de iteraciones. Repite mientras se cumple una condición booleana.
 
 Ambos bucles son fundamentales para controlar el flujo de ejecución en programas Java.
+        
+        
+        Crear 3 variables:
+        - Nombre
+        - Edad
+        - Salario
         */
+        System.out.println("VARIABLES");
+        System.out.println("----------");
+        int edad = 46;
+        String nombre = "Glenda,";
+        String salario = "$ 1000000"; 
+        
+        System.out.println("Mi nombre es "+nombre);
+        System.out.println("tengo "+edad+" años");
+        System.out.println("y mi salario es de "+salario);
+        
+        
         
         
     }
